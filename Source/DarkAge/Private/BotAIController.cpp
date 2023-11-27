be9d536a826c428e70fad753672d698807c13e75
Felipereira <73316197+paladino2077@users.noreply.github.com>
2023-11-27 15:46:55 -0300

    003

--- Source/DarkAge/Private/BotAIController.cpp
+++ Source/DarkAge/Private/BotAIController.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BotAIController.h"
 #include "Perception/PawnSensingComponent.h"
 #include "BehaviorTree/BehaviorTree.h"
@@ -9,13 +6,24 @@
 #include "GameFramework/Pawn.h"
 #include "BotCharacter.h"
 #include "Engine/Engine.h"
+#include "Arma.h"
 
 ABotAIController::ABotAIController()
 {
+	// construtor que cria o component de sensores do BotInimigo
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(FName("PawnSensingComp"));
+	
+	// aqui define como os sensores do inimigo deverão atuar
+	// deve fazer a varredura sensorial a cada 1/4 de segundo .25
 	PawnSensingComp->SensingInterval = .25f;
+
+	// detecção sensivel apenas de Players(Jogadores)
 	PawnSensingComp->bOnlySensePlayers = true;
+	
+	// o angulo de visão de detecção deve ser de 180 graus, isso facilita também ver as costas
 	PawnSensingComp->SetPeripheralVisionAngle(180.f); 
+
+	// o raio de visão será de 3000 unidades
 	PawnSensingComp->SightRadius = 3000.f;
 
 	BehaviorTreeComp = CreateDefaultSubobject<UBehaviorTreeComponent>(FName("BehaviorTreeComp"));
@@ -41,5 +49,7 @@ void ABotAIController::OnSeePawn(APawn* SensedPawn)
 		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("Eu estou te vendo"));
 		BlackBoardComp->SetValueAsObject("Inimigo", SensedPawn);
 		BlackBoardComp->SetValueAsBool("DevePerambular", false);
+		ABotCharacter* Bot = Cast<ABotCharacter>(GetPawn());
+		Bot->ArmaInimigo->Atirar();
 	}
 }
